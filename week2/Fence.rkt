917cdfdbb2352e45e6ddee9c565822c0bb3a3446
Leizer5 <leizer5@abv.bg>
2015-11-17 13:29:05 +0200

    Create Fence.rkt

---
+++ week2/Fence.rkt
@@ -0,0 +1,19 @@
+#lang racket
+;This function should take a string and concatenate it with itself n times.
+
+(define (string-repeat str n)
+  (define (string-iter i result)
+    (if (> i n) result
+        (string-iter (+ i 1) (string-append result str))))
+  (string-iter 1 ""))
+
+;We are going to put number in fences. Fences represent a string that looks something like this:  {-->n<--}
+
+(define (fence n)
+  (string-append "{"
+                 (string-repeat "-" (round (+ 1 (log n))))
+                 ">"
+                 (number->string n)
+                 "<"
+                 (string-repeat "-" (round (+ 1 (log n))))
+                 "}"))
