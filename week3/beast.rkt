76cfbeeeb144b465d2240fdf19310cba78b85680
Leizer5 <leizer5@abv.bg>
2015-11-18 12:04:50 +0200

    Update beast.rkt

--- week3/beast.rkt
+++ week3/beast.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require "fence.rkt")
+(require "week2/fence.rkt")
 
 ;Implement a function, called (nth-beast-number n), which returns the nth number of the beast.
 (define (nth-beast-number n)
