d3789cb8db73e0f354abe44317b5695d66070a89
Valeri Nikolov <leizer5@abv.bg>
2016-01-05 17:50:35 +0200

    Update tree-map.rkt

--- week7/tree-map.rkt
+++ week7/tree-map.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (require "tree.rkt")
-
+;The function takes a single argument funciton f and binarytreeand returns a new tree, where every node is transformed by f`
 ( define ( tree-map f tree )
    ( cond
       [ ( empty-tree? tree ) ( list ) ]
