c4169fbbd7613d3b3db96d88a3751d4d99cfcb9a
Valeri Nikolov <leizer5@abv.bg>
2016-01-25 20:22:49 +0200

    Update removeAt.hs

--- week11/removeAt.hs
+++ week11/removeAt.hs
@@ -1,5 +1,11 @@
 --Напишете функция removeAt която по даден индекс и списък, премахва елемента на индекса и връща новия списък.
-removeAt :: Int -> [a] -> [a]
-removeAt ind l
-    | ind < 0 || ind >= length l = error "Index out of bounds"
-    | otherwise = take ind l ++ drop (ind + 1) l
+removeAt n [] = error "The list is empty"
+removeAt 0 ys = tail ys
+removeAt n ys
+    |n > length ys && n < 0 = error "Integer out of bounds!"
+    |otherwise = iter (ys !! n) ys 
+        where
+            iter number [] = []
+            iter number xs
+                |not (number == head xs) = head xs : iter number (tail xs)
+                |otherwise = iter number (tail xs)
